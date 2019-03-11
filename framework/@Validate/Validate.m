55bcceed97f3b00c6c640242f73ae5e55f8f7e39
siru <lsr3406@163.com>
2019-03-11 13:56:56 +0800

    添加 Validate 类

---
+++ framework/@Validate/Validate.m
@@ -0,0 +1,19 @@
+% encoding: utf-8
+% @author: siru
+% @create on: 2019-03-05 15:52:03
+% @update on: 2019-03-05 15:53:32
+
+% 数据类型检验器
+classdef Validate < handle
+
+properties
+
+end
+
+methods (Static)
+
+% TODO methods
+
+end
+
+end
